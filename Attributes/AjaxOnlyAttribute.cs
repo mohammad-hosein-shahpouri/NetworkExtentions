d512db6165c41915512ebfe6a368517e3e4579bf
Mohammad Hosein Shahpouri <mh.shahpouri@gmail.com>
2022-01-07 00:25:55 +0330

    AjaxOnlyAttribute Created

---
+++ Attributes/AjaxOnlyAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace NetworkExtentions.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AjaxOnlyAttribute : ActionFilterAttribute
+{
+    private readonly string ajaxHeaderKey = "x-requested-with";
+    private readonly string ajaxHeaderValue = "XMLHttpRequest";
+
+    public override void OnActionExecuting(ActionExecutingContext filterContext)
+    {
+        ///x-requested-with: XMLHttpRequest
+        var headerValue = filterContext.HttpContext.Request.Headers[ajaxHeaderKey].ToString().ToLower();
+        if (headerValue == ajaxHeaderValue.ToLower()) base.OnActionExecuting(filterContext);
+        else filterContext.Result = new ForbidResult();
+    }
+}
